a93faee2582092566bab96ceffed5c4dbe705dbb
thinhanco <tranhuythinh2006@gmail.com>
2026-04-16 16:35:05 +0700

    Update NhanVien.cs

--- WebApplication10/Models/NhanVien.cs
+++ WebApplication10/Models/NhanVien.cs
@@ -1,4 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication10.Models
@@ -7,14 +7,23 @@ namespace WebApplication10.Models
     public class NhanVien
     {
         [Key]
+        [Column("MaNV")]
         public int MaNV { get; set; }
+
+        [Required(ErrorMessage = "Họ tên không được để trống")]
+        [StringLength(100, ErrorMessage = "Họ tên tối đa 100 ký tự")]
+        [Display(Name = "Họ và tên")]
+        [Column("HoTen")]
         public string HoTen { get; set; } = string.Empty;
+
+        [Display(Name = "Chức vụ")]
+        [Column("MaChucVu")]
         public int? MaChucVu { get; set; }
 
         [ForeignKey("MaChucVu")]
+        [Display(Name = "Chức vụ")]
         public virtual ChucVu? ChucVu { get; set; }
 
-        // Sửa dòng này để khớp với tên lớp ở Bước 1
         public virtual TaiKhoans? TaiKhoan { get; set; }
     }
 }
