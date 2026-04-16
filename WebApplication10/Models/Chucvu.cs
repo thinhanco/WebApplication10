6e37eb9479457680b88576b798c95df02250b7a8
thinhanco <tranhuythinh2006@gmail.com>
2026-04-16 16:34:41 +0700

    Update Chucvu.cs

--- WebApplication10/Models/Chucvu.cs
+++ WebApplication10/Models/Chucvu.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using WebApplication10.Controllers;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication10.Models
 {
+    [Table("ChucVus")]
     public class ChucVu
     {
         [Key]
+        [Column("MaChucVu")]
         public int MaChucVu { get; set; }
-        public string TenChucVu { get; set; } // "Admin" hoặc "NhanVien"
 
-        // Liên kết với bảng NhanVien
-        public virtual ICollection<NhanVien> NhanViens { get; set; }
+        [Required(ErrorMessage = "Tên chức vụ không được để trống")]
+        [StringLength(50, ErrorMessage = "Tên chức vụ tối đa 50 ký tự")]
+        [Display(Name = "Tên chức vụ")]
+        [Column("TenChucVu")]
+        public string TenChucVu { get; set; } = string.Empty;
+
+        public virtual ICollection<NhanVien> NhanViens { get; set; } = new List<NhanVien>();
     }
 }
