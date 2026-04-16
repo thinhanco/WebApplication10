bd70748eafd46b7d07903d2e6a22ac3f6d23b0a3
thinhanco <tranhuythinh2006@gmail.com>
2026-04-16 16:34:29 +0700

    Update CaLam.cs

--- WebApplication10/Models/CaLam.cs
+++ WebApplication10/Models/CaLam.cs
@@ -1,16 +1,35 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebApplication10.Models
 {
-    [Table("CaLam")]
+    [Table("CaLams")]
     public class CaLam
     {
         [Key]
+        [Column("MaCa")]
         public int MaCa { get; set; }
-        public string TenCa { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Tên ca")]
+        [Column("TenCa")]
+        public string TenCa { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Giờ bắt đầu")]
+        [Column("GioBatDau")]
         public TimeSpan GioBatDau { get; set; }
+
+        [Required]
+        [Display(Name = "Giờ kết thúc")]
+        [Column("GioKetThuc")]
         public TimeSpan GioKetThuc { get; set; }
+
+        [Required]
+        [Range(1, 50)]
+        [Display(Name = "Số lượng tối đa")]
+        [Column("SoLuongToiDa")]
         public int SoLuongToiDa { get; set; }
     }
 }
