72972b6c7af77ef720da7f6fcc4fcf68bdf322e2
thinhanco <tranhuythinh2006@gmail.com>
2026-04-16 16:34:52 +0700

    Update DangKiCaLam.cs

--- WebApplication10/Models/DangKiCaLam.cs
+++ WebApplication10/Models/DangKiCaLam.cs
@@ -1,20 +1,41 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using WebApplication10.Models;
 
-public class DangKyCaLam
+namespace WebApplication10.Models
 {
-    [Key]
-    public int MaDK { get; set; }
-    public int MaNV { get; set; }
-    public int MaCa { get; set; }
-    public DateTime NgayLam { get; set; }
+    [Table("DangKyCaLams")]
+    public class DangKyCaLam
+    {
+        [Key]
+        [Column("MaDK")]
+        public int MaDK { get; set; }
 
-    public string TrangThai { get; set; } = "Pending"; // Gán giá trị mặc định
-    public string? GhiChu { get; set; } // Thêm dấu ?
+        [Required]
+        [Column("MaNV")]
+        public int MaNV { get; set; }
 
-    [ForeignKey("MaNV")]
-    public virtual NhanVien? NhanVien { get; set; } // Thêm dấu ?
-    [ForeignKey("MaCa")]
-    public virtual CaLam? CaLam { get; set; } // Thêm dấu ?
+        [Required]
+        [Column("MaCa")]
+        public int MaCa { get; set; }
+
+        [Required]
+        [Column("NgayLam")]
+        [Display(Name = "Ngày làm")]
+        [DataType(DataType.Date)]
+        public DateTime NgayLam { get; set; }
+
+        [Column("TrangThai")]
+        [StringLength(20)]
+        public string TrangThai { get; set; } = "Pending";
+
+        [StringLength(200)]
+        [Display(Name = "Ghi chú")]
+        public string? GhiChu { get; set; }
+
+        [ForeignKey("MaNV")]
+        public virtual NhanVien? NhanVien { get; set; }
+
+        [ForeignKey("MaCa")]
+        public virtual CaLam? CaLam { get; set; }
+    }
 }
