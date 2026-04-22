50ded6b32a691ffe78ae1c17047aa53d2b00a0f1
thinhanco <tranhuythinh2006@gmail.com>
2026-04-22 10:50:49 +0700

    Update TaiKhoans.cs

--- WebApplication10/Models/TaiKhoans.cs
+++ WebApplication10/Models/TaiKhoans.cs
@@ -1,20 +1,44 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using WebApplication10.Models.UserModels;
 
 namespace WebApplication10.Models
 {
-    [Table("TaiKhoans")]
-    public class TaiKhoans
+    public enum UserRole
+    {
+        KhachHang,
+        NhanVien,
+        QuanTriVien
+    }
+    public class Account
     {
         [Key]
-        public string TenDangNhap { get; set; }
-        public string MatKhau { get; set; }
-        public int MaNV { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AccountID { get; set; }
 
-        // Dòng này để sửa lỗi Error 1 & 2
-        public bool IsLocked { get; set; } = false;
+        [Required(ErrorMessage = @"Tên đăng nhập không được để trống")]
+        [StringLength(50)]
+        [Column(TypeName = "nvarchar")]
+        public string Username { get; set; }
 
-        [ForeignKey("MaNV")]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [StringLength(100)]
+        [DataType(DataType.Password)]
+        [Column(TypeName = "nvarchar")]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Quyền truy cập không được để trống")]
+        [RegularExpression("^(KhachHang|NhanVien|QuanTriVien)$",
+            ErrorMessage = "Role phải là 'KhachHang', 'NhanVien' hoặc 'QuanTriVien'")]
+        public UserRole Role { get; set; }
+        [Required(ErrorMessage = "Email không được để trống")]
+        [Column(TypeName = "nvarchar")]
+        [StringLength(100)]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@gmail\.com$",
+            ErrorMessage = "Email phải có định dạng TenEmail@gmail.com và không chứa dấu cách")]
+        public string Email { get; set; }
+
         public virtual NhanVien NhanVien { get; set; }
+        public virtual BaseUser UserProfile { get; set; }
     }
+
 }
