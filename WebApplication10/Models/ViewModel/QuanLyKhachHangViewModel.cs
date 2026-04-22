5add20af307404281a8dc307c23f733b326b3cc6
thinhanco <tranhuythinh2006@gmail.com>
2026-04-22 21:50:09 +0700

    Update QuanLyKhachHangViewModel.cs

--- WebApplication10/Models/ViewModel/QuanLyKhachHangViewModel.cs
+++ WebApplication10/Models/ViewModel/QuanLyKhachHangViewModel.cs
@@ -1,1 +1,16 @@
 
+namespace WebApplication10.Models.ViewModels
+{
+    public class QuanLyKhachHangViewModel
+
+    {
+        public int BookingID { get; set; }
+        public string TenKhachHang { get; set; }
+        public string SoDienThoai { get; set; }
+        public string SoPhong { get; set; }
+        public string NgayDen { get; set; }
+        public string TrangThaiHienThi { get; set; }
+        public string BadgeClass { get; set; } // bg-warning, bg-success...
+        public bool ChoPhepDuyet { get; set; }
+    }
+}
