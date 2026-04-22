3eb6c5c8204b2e0363209014955c1ac9ea3070a1
thinhanco <tranhuythinh2006@gmail.com>
2026-04-22 21:50:37 +0700

    Create QuanLyPhongViewModel.cs

---
+++ WebApplication10/Models/ViewModel/QuanLyPhongViewModel.cs
@@ -0,0 +1,11 @@
+namespace WebApplication10.Models.ViewModels
+{
+    public class QuanLyPhongViewModel
+    {
+        public int SoPhong { get; set; }
+        public string LoaiPhong { get; set; }
+        public string TrangThaiDisplay { get; set; }
+        public string BadgeClass { get; set; }
+        public string CurrentStatusValue { get; set; } // Dùng để chọn sẵn tro
+    }
+}
